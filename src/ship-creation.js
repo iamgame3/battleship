c9a5bb2ac6339264ec9b01c7e0220131c5dba8c7
iamgame3 <kosmosworld9@hotmail.com>
2023-04-02 13:59:49 -0400

    Add ship factory function and Jest test file

---
+++ src/ship-creation.js
@@ -0,0 +1,13 @@
+const createShip = (shipLength) => ({
+  length: shipLength,
+  hits: 0,
+  sunk: false,
+  isSunk() {
+    if (this.hits === this.length) return true;
+    return false;
+  },
+  hit() {
+    this.hits += 1;
+    if (this.isSunk() === true) this.sunk = true;
+  },
+});
