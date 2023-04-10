7ffd5db98f12b826146d5b64b908b07edb5069a5
iamgame3 <kosmosworld9@hotmail.com>
2023-04-10 19:56:36 -0400

    Add symbol legend and player grids

--- src/DOM-gameboard.js
+++ src/DOM-gameboard.js
@@ -1,20 +1,48 @@
 const gameSetup = () => {
   const gameStartButton = document.querySelector(".game-start-button");
   const body = document.querySelector("body");
+  const pageContainer = document.querySelector(".page-container");
 
   gameStartButton.addEventListener("click", () => {
-    body.replaceChildren();
+    pageContainer.replaceChildren();
     const legend = document.createElement("div");
     const missLegend = document.createElement("h2");
     const hitLegend = document.createElement("h2");
     missLegend.textContent = "/: MISS";
     hitLegend.textContent = "X: HIT";
-    missLegend.classList.add("legend-header");
-    hitLegend.classList.add("legend-header");
+    missLegend.classList.add("legend-item");
+    hitLegend.classList.add("legend-item");
     legend.classList.add("legend");
     legend.appendChild(missLegend);
     legend.appendChild(hitLegend);
     body.appendChild(legend);
+    const gameGridsContainer = document.createElement("div");
+    const enemyGrid = document.createElement("div");
+    const yourGrid = document.createElement("div");
+    for (let i = 0; i < 100; i += 1) {
+      const square = document.createElement("div");
+      square.classList.add("normal-square");
+      enemyGrid.appendChild(square);
+    }
+    enemyGrid.classList.add("enemy-grid");
+    const enemyGridHeader = document.createElement("h2");
+    enemyGridHeader.textContent = "ENEMY GRID";
+    enemyGridHeader.classList.add("enemy-grid-header");
+    enemyGrid.appendChild(enemyGridHeader);
+    gameGridsContainer.classList.add("game-grids-container");
+    for (let i = 0; i < 100; i += 1) {
+      const square = document.createElement("div");
+      square.classList.add("normal-square");
+      yourGrid.appendChild(square);
+    }
+    yourGrid.classList.add("your-grid");
+    const yourGridHeader = document.createElement("h2");
+    yourGridHeader.textContent = "YOUR GRID";
+    yourGridHeader.classList.add("your-grid-header");
+    yourGrid.appendChild(yourGridHeader);
+    gameGridsContainer.appendChild(enemyGrid);
+    gameGridsContainer.appendChild(yourGrid);
+    pageContainer.appendChild(gameGridsContainer);
   });
 };
 
